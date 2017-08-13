24b532976ee9300f0318930f6dba7f87eb30c900
Adrian Parvin D. Ouano <adrianparvino@gmail.com>
2017-08-13 20:25:07 +0800

    Made some revisions to build environment

--- config.nix
+++ config.nix
@@ -1,25 +1,16 @@
 compiler: {
   packageOverrides = pkgs: rec {
+    webkitgtk = pkgs.webkitgtk216x;
     haskellPackages = pkgs.haskell.packages.${compiler}.override {
       overrides = self: super: {
         hlint = self.callPackage ./pkgs/hlint.nix {};
-	      jsaddle = self.callPackage ./pkgs/jsaddle.nix {};
+        jsaddle = self.callPackage ./pkgs/jsaddle.nix {};
         reflex = self.callPackage (pkgs.fetchFromGitHub {
           owner = "reflex-frp";
           repo = "reflex";
           rev = "908348b4799caec260e083568bace846d7fe5d80";
           sha256 = "13r7sk0xj0nnzwpmdqgp5ny93pdqq3d3h9w3mj84aiy5xhiakjvm";
         }) {};
-	  	
-        # reflex-dom = let
-        #   src = pkgs.fetchFromGitHub {
-        #     owner = "reflex-frp";
-        #     repo = "reflex-dom";
-        #     rev = "d7b859bccd0d9c57bd9635de75b3ca9d2ca79568";
-        #     sha256 = "11ljk6hpsqw0as2iaa5m3pbb4qfgz8bzdgs64c2ld1cfp09wyz2s";
-        #   };
-        #   foo = import src self pkgs;
-        # in foo.reflex-dom-core;
       } // (import (pkgs.fetchFromGitHub {
          owner = "reflex-frp";
          repo = "reflex-dom";
--- default.nix
+++ default.nix
@@ -6,17 +6,42 @@ let
   
   # hlint = runCommand "hlint-1.9.0" "cabal2nix --no-check cabal://hlint-1.9.0";
   # haskellPackages' = haskellPackages.override {overrides = (self: super: {inherit hlint; }); };
-  ghc = pkgs.haskellPackages.ghcWithPackages paths;
-in pkgs.stdenv.mkDerivation {
-  name = "cellgame";
-  src = ./src;
+#  ghc = pkgs.haskellPackages.ghcWithPackages paths;
+in pkgs.haskellPackages.mkDerivation {
+  pname = "CellGame";
+  version = "0.1.0.0";
+  src = ./.;
 
-  dontBuild = true;
-  buildInputs = [ ghc ];
+  executableHaskellDepends = with pkgs.haskellPackages; [
+    base reflex reflex-dom clay font-awesome-type
+    containers text
+  ];
 
-  installPhase = ''
-    mkdir -pv $out
-    ghcjs ./Main.hs -o $out/CellGame -odir $TMPDIR -hidir $TMPDIR
-    echo "<html> <head> <script src=\"CellGame.jsexe/all.js\"></script> </head> </html>" > $out/index.html
+  buildTools = [ pkgs.closurecompiler ];
+  
+  postInstall = ''
+    closure-compiler -O ADVANCED --js $out/bin/CellGame.jsexe/all.js --js_output_file $out/all.min.js
+    echo "<html> <head> <script src=\"all.min.js\"></script> </head> </html>" > $out/index.html
+    rm -r $out/bin/CellGame.jsexe
+    rm $out/bin/CellGame
   '';
+
+  license = pkgs.stdenv.lib.licenses.gpl3;
+
+  isExecutable = true;
 }
+
+# pkgs.stdenv.mkDerivation {
+#   name = "cellgame";
+#   src = ./src;
+# 
+#   dontBuild = true;
+#   buildInputs = [ ghc pkgs.closurecompiler ];
+# 
+#   installPhase = ''
+#     mkdir -pv $out
+#     ghcjs ./Main.hs -o $TMPDIR/CellGame.exejs -odir $TMPDIR -hidir $TMPDIR
+#     echo "<html> <head> <script src=\"all.min.js\"></script> </head> </html>" > $out/index.html
+#     closure-compiler -O ADVANCED --js $TMPDIR/CellGame.exejs/all.js --js_output_file $out/all.min.js
+#   '';
+# }
